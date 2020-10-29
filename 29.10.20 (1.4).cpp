6cafe8eee6f9efd3fe8b6383f3a2845f77bafa2e
alina-gai <73353147+alina-gai@users.noreply.github.com>
2020-10-29 22:43:53 +0300

    Add files via upload

---
+++ 29.10.20 (1.4).cpp
@@ -0,0 +1,46 @@
+﻿
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+   /* double y, F;
+    const int n = 20;
+    int i, j;
+    double x[] = { -0.1, -10.5, 5.8 };
+    int u[n+1];
+    u[0] = 1;
+    for (i = 0; i < 3; i++)
+    {
+        F = sin(x[i] * x[i] + x[i] + 1) / (1 + x[i] * x[i]);
+        for (j = 1; j <= n; j++)
+        {
+            u[j] = 2*u[j-1] / 3 + F/ (3 * u[j - 1] * u[j - 1]);
+        }
+        y = u[n];
+        cout << y << " ";
+    }*/
+
+    /*double y, F;
+    const int n = 23;
+    int i, j;
+    double x[] = { -0.4, 2.4, 5.0 };
+    int u[n + 1];
+    u[0] = 1;
+    for (i = 0; i < 3; i++)
+    {
+        F = 3.5 * sin(x[i]) * sin(x[i]) - 2.7 * sin(x[i]) + 1;
+        for (j = 1; j <= n; j++)
+        {
+            u[j] = 2 * u[j - 1] / 3 + F / (3 * u[j - 1] * u[j - 1]);
+        }
+        y = u[n];
+        cout << y << " ";
+    }
+    system("pause");
+    return 0;*/
+}
+
+
